01a880bc7fae7462117b62f249b754dfc899d783
sauleric34-hub <sauleric34@gmail.com>
2026-05-28 09:34:28 -0500

    Build core AdminLayout part 1

---
+++ src/components/CoreAdminLayout_Boss_1.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export const CoreAdminLayout_Boss_1 = () => {
+    return (
+        <div>
+            <h1>Core Layout 1 by sauleric34-hub</h1>
+            <p>Managing the entire application.</p>
+        </div>
+    );
+};
